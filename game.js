787621398c1a1722ce98f7a0866e36ff1e0a6569
moesarsour <SarsourM@gmail.com>
2023-02-04 11:12:30 -0500

    Added Ball Score and Paddle Code

--- game.js
+++ game.js
@@ -7,4 +7,27 @@ const gameHeight = gameBoard.height;
 const boardBackground = "lightblue";
 const paddle1Color = "black";
 const paddle2Color = "white";
-const paddleBorder = "solid";
+const paddleBorder = "red";
+const ballColor = "green";
+const ballRadius = "12.5";
+const paddleSpeed = 50;
+let intervalID;
+let ballSpeed = 1;
+let ballX = gameWidth / 2;
+let ballY = gameHeight / 2;
+let ballXDirection = 0;
+let ballYDirection = 0;
+let player1Score = 0;
+let player2Score = 0;
+let paddle1 = {
+    width: 25,
+    height: 100,
+    x: 0,
+    y: 0
+};
+let paddle2 = {
+    width: 25,
+    height: 100,
+    x: gameWidth - 25,
+    y: gameHeight - 100
+};
